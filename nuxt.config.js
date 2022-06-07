63d68488818fc0a2e63994f8da36524e5d563c78
fajarrosi <fajarilhamrosi@gmail.com>
2022-06-07 07:32:12 +0700

    test lagi

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/Portfolio/'
+  },
   // <link rel="preconnect" href="https://fonts.googleapis.com">
   // <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   // <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
