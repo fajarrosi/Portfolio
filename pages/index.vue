ae6ead4f4a58d5f06dcd83eaa66bcfa2aa60d955
fajarrosi <fajarilhamrosi@gmail.com>
2022-06-08 07:00:24 +0700

    add to github and init project

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,25 @@
 <template>
 <v-app>
   <v-main class="bg-grey">
-    <v-container  fluid>
+    <v-container fluid>
       <v-row align="center" style="height:100vh;">
-        <v-col cols="12" sm="8" md="6">
+        <v-col cols="12" md="6">
           <v-row justify="center" justify-md="end">
-            <v-col md="6" cols="11">
+            <v-col md="6" cols="11" class="text-center text-md-left">
               <h1 class="primary--text">Fajar Ilham Rosi</h1>
               <h3>I'm <span class="secondary--text"> Frontend Developer </span></h3>
-              <v-row class="mt-2">
+              <v-row>
                 <v-col>
                   <v-btn
+                    depressed
+                    color="primary"
                     fab
-                    dark
+                    dense
                     small
-                    color="pink"
-                  >
-                    <v-img src="~assets/twitter.svg" />
-                  </v-btn>
-                  <v-btn
-                    class="mx-2"
-                    fab
-                    dark
-                    small
-                    color="pink"
+                    @click="openGithub"
                   >
                     <v-icon dark>
-                      mdi-heart
+                      mdi-github
                     </v-icon>
                   </v-btn>
                 </v-col>
@@ -35,12 +28,29 @@
             </v-col>
           </v-row>
         </v-col>
-        <v-col cols="12" md="6">
-          <v-row align="center" justify="center" justify-md="start">
-            <v-col cols="7">
-              <img src="~assets/beranda.png" alt="ilustrasi beranda" srcset="~assets/beranda.png" width="100%" height="100%">
-            </v-col>
-          </v-row>
+        <v-col cols="12" md="6" class="text-center text-md-left">
+          <img src="~assets/beranda.png" alt="ilustrasi beranda" srcset="~assets/beranda.png" width="100%" height="100%" style="max-width:400px;">
+        </v-col>
+        <v-col cols="12" class="text-center">
+          <v-btn
+            depressed
+            color="primary"
+            fab
+            dense
+            small
+            @click="scrollTo"
+          >
+            <v-icon dark>
+              mdi-chevron-double-down
+            </v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container>
+      <v-row id="project" style="min-height:600px;">
+        <v-col class="text-center">
+          <h1 class="primary--text">Projects I've Done</h1>
         </v-col>
       </v-row>
     </v-container>
@@ -50,6 +60,14 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  methods: {
+    openGithub() {
+      window.open("https://github.com/fajarrosi",'_blank');
+    },
+    scrollTo() {
+      window.open("#project",'_self');
+    }
+  }
 }
 </script>
